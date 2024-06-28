6de36fc063963d80f3f4b229d188630f54d7e593
unknown <qaysarovmirzoxid@gmail.com>
2024-06-28 18:07:53 +0500

    I create

---
+++ script.js
@@ -0,0 +1,18 @@
+/*var email = prompt("Emayilingni kiriting");
+
+var emailhidden = email.slice(3, -10);
+
+var hiddenemail = email.replace(emailhidden, "*****");
+
+alert(hiddenemail); */
+
+
+var text = prompt ("Enter the result")
+
+var againtext = prompt ("text")
+
+var newtext = prompt ("input newtext") 
+
+var chengetext = text.replace(againtext, newtext)
+
+console.log(chengetext);
